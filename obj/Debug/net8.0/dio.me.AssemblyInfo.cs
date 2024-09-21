2d952a05bdc429b46df08783bd4fedc725bfa09c
nome de usuário do GitHub <Email do GitHub>
2024-09-21 02:57:39 -0300

    Update Program.cs-2

--- obj/Debug/net8.0/dio.me.AssemblyInfo.cs
+++ obj/Debug/net8.0/dio.me.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dio.me")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec73081777ea78a5ab52e553473a1990652db13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4141a35f9477070762f3a42808e78d6fa0741935")]
 [assembly: System.Reflection.AssemblyProductAttribute("dio.me")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dio.me")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
